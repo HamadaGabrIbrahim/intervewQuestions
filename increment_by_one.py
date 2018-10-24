da9f59d72fe8af2d2e44767e32123bb8bb9ed7d4
Hamada Ibrahim <Hamada.G.Ibrahim@gmail.com>
2018-10-23 19:41:00 -0700

    Add comments explaining the logic of the problem Add unit tests to test the problem

--- increment_by_one.py
+++ increment_by_one.py
@@ -1,18 +1,24 @@
 """
 Given a number represented as a list, increment the number by one.
 
-input [2, 7, 9]
-output [2, 8, 0]
-
 input [3, 6, 8]
+368 + 1 = 369
+
 output [3, 6, 9]
 
+
+input [2, 7, 9]
+279 + 1 = 280
+output [2, 8, 0]
+
+
 input [9, 9, 9, 9]
+9999 + 1 = 10000
 output [1, 0, 0, 0, 0]
+
 
 Time complexity is O(n)
 """
-
 
 def increment_by_one(num_as_list):
     num_as_list[-1] += 1
@@ -25,3 +31,10 @@ def increment_by_one(num_as_list):
         num_as_list[0] = 0
         num_as_list.insert(0, 1)
     return num_as_list
+
+print(increment_by_one([3, 6, 8]))
+print(increment_by_one([2, 7, 9]))
+print(increment_by_one([9, 9, 9]))
+
+
+
